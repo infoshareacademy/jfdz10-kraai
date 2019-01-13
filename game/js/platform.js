c8cf5211950ed1b3287640b02cfb3ba414cf29e7
Jakub Rohde <rohdejakub@gmail.com>
2019-01-13 20:54:42 +0100

    obstacle creator almost done

--- game/js/platform.js
+++ game/js/platform.js
@@ -1,27 +1,48 @@
-var htmlObjects = {
+const htmlObjects = {
   viewport: document.querySelector(".viewport"),
   platform: document.querySelector(".platform"),
   obstacleElement: document.querySelectorAll(".obstacle")
 };
+const gameConfiguration = {
+  gameSpeed : 1,
+  obstacleSpeed(){ return Math.random() * 1500 },
+  viewportWidth : htmlObjects.viewport.offsetWidth,
+}
 
-const obstacle = htmlObjects.obstacleElement;
-const platform = htmlObjects.platform;
-const maxObstacles = 4;
-var obstacleCount = 0;
+let obstacle = htmlObjects.obstacleElement;
+let platform = htmlObjects.platform;
+let obstacleCount = 0;
 
 function obstacleCreator() {
-  if (obstacle.length <= maxObstacles) {
+
     let newObstacle = document.createElement("div");
+    newObstacle.style.left = `${gameConfiguration.viewportWidth + 50 - platform.offsetLeft}px`;
     newObstacle.classList.add("obstacle");
     platform.appendChild(newObstacle);
+    gameConfiguration.obstacleSpeed = 
     countObstacle(1);
-  }
+ 
 }
 
 function countObstacle(amount) {
   obstacleCount = obstacleCount + amount;
+  htmlObjects.obstacleElement = document.querySelectorAll('.obstacle');
+  obstacle = htmlObjects.obstacleElement;
 }
 
+function gameMove(){
+  const moveInterval = setInterval(() => {
+   platform.style.left = `${platform.offsetLeft - 2}px`;
+ },10)
 
+}
+
+gameMove();
+
+setInterval(() => {
+  obstacleCreator();
+  
+  
+}, gameConfiguration.obstacleSpeed());
 
 

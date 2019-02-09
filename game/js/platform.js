b94827bbf1e20776e9cf045af532a08c53bc5f08
Daria Kozielecka <dkozielecka@MacBook-Air-Daria.local>
2019-02-09 18:42:16 +0100

    fix 1

--- game/js/platform.js
+++ game/js/platform.js
@@ -8,7 +8,7 @@ const gameStart = () => {
 
   const htmlObjects = {
     viewport: document.querySelector(".viewport"),
-    platform: document.querySelector('.platform'),
+    platform: document.querySelector(".platform"),
     platform1: document.querySelector(".platform1"),
     platform2: document.querySelector(".platform2"),
     platform3: document.querySelector(".platform3"),
@@ -22,43 +22,49 @@ const gameStart = () => {
     backgroundLayer3: document.querySelector(".layer3"),
     character: document.querySelector(".character"),
     getObstacleElements() {
-      return document.querySelectorAll(".obstacle")
+      return document.querySelectorAll(".obstacle");
     },
     getObstaclePositions() {
-      return Array.from(this.getObstacleElements())
-        .map(element => ({
-          width: element.offsetWidth,
-          height: element.offsetHeight,
-          left: element.offsetLeft,
-          right: element.offsetLeft + element.offsetWidth,
-          top: element.offsetTop,
-          down: element.offsetTop + element.offsetHeight,
-        }))
+      return Array.from(this.getObstacleElements()).map(element => ({
+        width: element.offsetWidth,
+        height: element.offsetHeight,
+        left: element.offsetLeft,
+        right: element.offsetLeft + element.offsetWidth,
+        top: element.offsetTop,
+        down: element.offsetTop + element.offsetHeight
+      }));
     },
     getCandyElements() {
-      return document.querySelectorAll(".candy")
+      return document.querySelectorAll(".candy");
     },
     getCandyPositions() {
-      return Array.from(this.getCandyElements())
-        .map(element => ({
-          width: element.offsetWidth,
-          height: element.offsetHeight,
-          left: element.offsetLeft,
-          right: element.offsetLeft + element.offsetWidth,
-          top: element.offsetTop,
-          down: element.offsetTop + element.offsetHeight,
-        }))
+      return Array.from(this.getCandyElements()).map(element, index => ({
+        width: element.offsetWidth,
+        height: element.offsetHeight,
+        left: element.offsetLeft,
+        right: element.offsetLeft + element.offsetWidth,
+        top: element.offsetTop,
+        down: element.offsetTop + element.offsetHeight,
+        index: index
+      }));
     },
     getCharacterPosition() {
       return {
         width: this.character.offsetWidth - 10,
         height: this.character.offsetHeight,
         left: this.character.offsetLeft - this.platform.offsetLeft,
-        right: this.character.offsetLeft + this.character.offsetWidth - this.platform.offsetLeft + 30,
-        top: this.character.offsetTop - this.background.offsetHeight + this.platform.offsetHeight,
-        down: this.character.offsetTop + this.character.offsetHeight,
-      }
-    },
+        right:
+          this.character.offsetLeft +
+          this.character.offsetWidth -
+          this.platform.offsetLeft +
+          30,
+        top:
+          this.character.offsetTop -
+          this.background.offsetHeight +
+          this.platform.offsetHeight,
+        down: this.character.offsetTop + this.character.offsetHeight
+      };
+    }
   };
 
   htmlObjects.scoreCounter.textContent = score;
@@ -68,15 +74,14 @@ const gameStart = () => {
     htmlObjects.platform.style.left = 0;
     htmlObjects.getCandyElements().forEach(element => element.remove());
     htmlObjects.getObstacleElements().forEach(element => element.remove());
-    htmlObjects.levelCounter.classList.remove('display-none');
-    htmlObjects.scoreCounter.classList.remove('display-none');
+    htmlObjects.levelCounter.classList.remove("display-none");
+    htmlObjects.scoreCounter.classList.remove("display-none");
     htmlObjects.menu.classList.add("display-none");
-
   };
   gameRestart();
 
-
-  const levelDisplay = () => htmlObjects.levelCounter.innerHTML = `Level ${level}`;
+  const levelDisplay = () =>
+    (htmlObjects.levelCounter.innerHTML = `Level ${level}`);
 
   let gameConfiguration = {
     minDistanceBetweenObstacles: 800,
@@ -90,9 +95,9 @@ const gameStart = () => {
     platformWidth: htmlObjects.platform.offsetWidth,
     isPlatformEnd: false,
     gameSpeed() {
-      return 5 * speedLevel
+      return 5 * speedLevel;
     }
-  }
+  };
 
   const obstacleCreator = () => {
     let minimalDistance = gameConfiguration.minDistanceBetweenObstacles;
@@ -104,20 +109,28 @@ const gameStart = () => {
       newObstacle.style.left = `${obstaclePositionOnPlatform}px`;
       newObstacle.classList.add("obstacle");
       htmlObjects.platform.appendChild(newObstacle);
-      obstaclePositionOnPlatform = obstaclePositionOnPlatform + minimalDistance + Math.random() * (maximalDistance - minimalDistance);
-    };
+      obstaclePositionOnPlatform =
+        obstaclePositionOnPlatform +
+        minimalDistance +
+        Math.random() * (maximalDistance - minimalDistance);
+    }
   };
 
-  const candyCreator = (time) => {
-
+  const candyCreator = time => {
     let candyTimeout = setTimeout(() => {
       if (gameOnOrOff === 0) {
-        clearTimeout(candyTimeout)
+        clearTimeout(candyTimeout);
       } else {
-        ;
         let minimalDistance = gameConfiguration.minDistanceBetweenCandy;
-        let elementLeft = htmlObjects.platform.offsetLeft * -1 + gameConfiguration.viewportWidth + minimalDistance;
-        for (let i = 0; i < Math.random() * gameConfiguration.maxCandySet; i++) {
+        let elementLeft =
+          htmlObjects.platform.offsetLeft * -1 +
+          gameConfiguration.viewportWidth +
+          minimalDistance;
+        for (
+          let i = 0;
+          i < Math.random() * gameConfiguration.maxCandySet;
+          i++
+        ) {
           let newCandy = document.createElement("div");
           newCandy.style.left = `${elementLeft}px`;
           newCandy.classList.add("candy");
@@ -128,7 +141,7 @@ const gameStart = () => {
             left: newCandy.offsetLeft,
             right: newCandy.offsetLeft + newCandy.offsetWidth,
             top: newCandy.offsetTop,
-            down: newCandy.offsetTop + newCandy.offsetHeight,
+            down: newCandy.offsetTop + newCandy.offsetHeight
           };
           htmlObjects.getObstaclePositions().forEach(obstacle => {
             if (checkCollisionX(newCandyPosition, obstacle)) {
@@ -136,7 +149,7 @@ const gameStart = () => {
             }
           });
           elementLeft += minimalDistance;
-        };
+        }
         candyCreator(Math.random() * 3000 + 1000);
       }
     }, time);
@@ -144,68 +157,104 @@ const gameStart = () => {
 
   const gameMove = () => {
     const moveInterval = setInterval(() => {
-      htmlObjects.platform.style.left = `${htmlObjects.platform.offsetLeft - gameConfiguration.gameSpeed()}px`;
-      htmlObjects.backgroundLayer1.style.backgroundPositionX = `${parseFloat(window.getComputedStyle(htmlObjects.backgroundLayer1).backgroundPositionX) - gameConfiguration.gameSpeed() /8}px`;
-      htmlObjects.backgroundLayer2.style.backgroundPositionX = `${parseFloat(window.getComputedStyle(htmlObjects.backgroundLayer2).backgroundPositionX) - gameConfiguration.gameSpeed() /6}px`;
-      htmlObjects.backgroundLayer3.style.backgroundPositionX = `${parseFloat(window.getComputedStyle(htmlObjects.backgroundLayer3).backgroundPositionX) - gameConfiguration.gameSpeed() /4}px`;
+      htmlObjects.platform.style.left = `${htmlObjects.platform.offsetLeft -
+        gameConfiguration.gameSpeed()}px`;
+      htmlObjects.backgroundLayer1.style.backgroundPositionX = `${parseFloat(
+        window.getComputedStyle(htmlObjects.backgroundLayer1)
+          .backgroundPositionX
+      ) -
+        gameConfiguration.gameSpeed() / 8}px`;
+      htmlObjects.backgroundLayer2.style.backgroundPositionX = `${parseFloat(
+        window.getComputedStyle(htmlObjects.backgroundLayer2)
+          .backgroundPositionX
+      ) -
+        gameConfiguration.gameSpeed() / 6}px`;
+      htmlObjects.backgroundLayer3.style.backgroundPositionX = `${parseFloat(
+        window.getComputedStyle(htmlObjects.backgroundLayer3)
+          .backgroundPositionX
+      ) -
+        gameConfiguration.gameSpeed() / 4}px`;
       obstacleColision(moveInterval);
       levelUp();
-    }, 10)
-
-  }
-  const obstacleColision = (interval) => {
+    }, 10);
+  };
+  const obstacleColision = interval => {
     htmlObjects.getObstaclePositions().forEach(obstacle => {
-      if (checkCollisionX(htmlObjects.getCharacterPosition(), obstacle) && checkCollisionY(htmlObjects.getCharacterPosition(), obstacle)) {
+      if (
+        checkCollisionX(htmlObjects.getCharacterPosition(), obstacle) &&
+        checkCollisionY(htmlObjects.getCharacterPosition(), obstacle)
+      ) {
         clearInterval(interval);
         gameOver();
       }
     });
-  }
+  };
 
   const candyColision = () => {
     htmlObjects.getCandyPositions().forEach(candy => {
-      if (checkCollisionX(htmlObjects.getCharacterPosition(), candy) && checkCollisionY(htmlObjects.getCharacterPosition(), candy)) {
+      if (
+        checkCollisionX(htmlObjects.getCharacterPosition(), candy) &&
+        checkCollisionY(htmlObjects.getCharacterPosition(), candy)
+      ) {
         score += 1;
         htmlObjects.scoreCounter.textContent = score;
-
+        getCandyElements()[index].remove();
       }
     });
-  }
+  };
 
   const candyPointsCounter = () => {
     setInterval(candyColision, 100);
-  }
+  };
 
   const gameOver = () => {
     gameOnOrOff = 0;
     time = 1000;
-    htmlObjects.levelCounter.classList.add('display-none');
-    htmlObjects.scoreCounter.classList.add('display-none');
+    htmlObjects.levelCounter.classList.add("display-none");
+    htmlObjects.scoreCounter.classList.add("display-none");
     htmlObjects.menu.classList.remove("display-none");
-  }
-
+  };
 
   const checkCollisionX = (objectX, objectY) => {
-    return (objectY.left - objectX.left >= 0 && objectY.left - objectX.left <= objectX.width) || (objectX.right - objectY.right >= 0 && objectX.right - objectY.right <= objectX.width)
-  }
+    return (
+      (objectY.left - objectX.left >= 0 &&
+        objectY.left - objectX.left <= objectX.width) ||
+      (objectX.right - objectY.right >= 0 &&
+        objectX.right - objectY.right <= objectX.width)
+    );
+  };
 
   const checkCollisionY = (objectX, objectY) => {
-    return (objectY.top - objectX.top >= 0 && objectY.top - objectX.top <= objectX.height) || (objectX.down - objectY.down >= 0 && objectX.down - objectY.down <= objectX.height)
-  }
+    return (
+      (objectY.top - objectX.top >= 0 &&
+        objectY.top - objectX.top <= objectX.height) ||
+      (objectX.down - objectY.down >= 0 &&
+        objectX.down - objectY.down <= objectX.height)
+    );
+  };
 
   const levelUp = () => {
-    if (htmlObjects.platform2.offsetLeft + htmlObjects.platform.offsetLeft < 0) {
+    if (
+      htmlObjects.platform2.offsetLeft + htmlObjects.platform.offsetLeft <
+      0
+    ) {
       speedLevel = 1.5;
-      level = 2
-    };
-    if (htmlObjects.platform3.offsetLeft + htmlObjects.platform.offsetLeft < 0) {
+      level = 2;
+    }
+    if (
+      htmlObjects.platform3.offsetLeft + htmlObjects.platform.offsetLeft <
+      0
+    ) {
       speedLevel = 2;
       level = 3;
-    };
-    if (htmlObjects.platform4.offsetLeft + htmlObjects.platform.offsetLeft < 0) {
+    }
+    if (
+      htmlObjects.platform4.offsetLeft + htmlObjects.platform.offsetLeft <
+      0
+    ) {
       speedLevel = 2.5;
       level = 4;
-    };
+    }
     levelDisplay();
   };
 
@@ -214,4 +263,4 @@ const gameStart = () => {
   gameMove();
   levelDisplay();
   candyPointsCounter();
-}
+};
